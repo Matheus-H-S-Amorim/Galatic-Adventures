9bb5a6db89701a69141b3c3596d67aec3b44c86b
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-05-06 17:00:38 -0300

    importa pacote pygame e faz janela do jogo

---
+++ testesmat.py
@@ -0,0 +1,12 @@
+
+# INICIALIZAÇÃO 
+
+#Importa e inicia pacotes 
+import pygame 
+pygame.init()
+
+#Gera tela principal
+window = pygame.display.set_mode((600, 300))
+pygame.display.set_caption('Jogo do Astronauta!')
+
+game = True 
