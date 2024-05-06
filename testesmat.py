7efc55ca8db136b9c6534efc5e15c7ef5272b595
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-05-06 20:08:41 -0300

    imagens de fundo e do personagem que agora se move com setas do teclados

--- testesmat.py
+++ testesmat.py
@@ -5,9 +5,37 @@
 import pygame 
 pygame.init()
 
+
 #Gera tela principal
-window = pygame.display.set_mode((600, 300))
+WIDTH = 1300
+HEIGHT = 650
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jogo do Astronauta!')
+
+
+#Inicia assests
+player_WIDTH= 200
+player_HEIGHT = 200
+
+font = pygame.font.SysFont(None,48)
+background = pygame.image.load('assets/img/fundo_campo.jpg').convert()
+background_small= pygame.transform.scale(background, (WIDTH,HEIGHT))
+
+player_img = pygame.image.load('assets/img/menino_bone.png').convert_alpha()
+player_img_small= pygame.transform.scale(player_img, (player_WIDTH, player_HEIGHT))
+
+
+#player configurações
+player_x = 0
+player_y = HEIGHT-player_HEIGHT-60
+player_speedx = 0
+player_speedy = 0
+
+# Controlador de velocidade do jogo 
+clock = pygame.time.Clock()
+FPS = 30
+
+
 
 # Inicia jogo 
 game = True 
@@ -15,19 +43,45 @@ game = True
 
 # Loop do jogo 
 while game: 
+    clock.tick(FPS)  # velocidade do jogo 
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             game = False 
-        if event.type== pygame.KEYUP:
-            game = False
+
+    
+    # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player_speedx -= 8
+            if event.key == pygame.K_RIGHT:
+                player_speedx += 8
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player_speedx += 8
+            if event.key == pygame.K_RIGHT:
+                player_speedx -= 8
+
     
     # Gera saídas
-    window.fill((0, 0, 255))  # Preenche com a cor branca
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+
+    player_x += player_speedx
+    player_y += player_speedy
+    
+    window.blit(background_small, (0, 0))                   # plota background 
+    window.blit(player_img_small, (player_x, player_y))     #plota personagem
+
 
     # Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # Finalização 
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+
+
 
