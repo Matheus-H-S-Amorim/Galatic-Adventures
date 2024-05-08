0f4c716e80ad400b813d79bef708f4ecacbc6bc5
Matheus-H-S-Amorim <matheushsa@al.insper.edu.br>
2024-05-08 18:53:45 -0300

    cria config e preenche assets com dicionario

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import path 
+
+# ATALHOS para Pastas com Figuras e Sons 
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+#FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
